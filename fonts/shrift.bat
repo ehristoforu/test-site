6bbe3acb53b5e1bd3a94a1ab1396382148773cce
Evgeniy Hristoforu <evgenijhristo87@gmail.com>
2024-03-23 17:09:54 +0300

    init

---
+++ fonts/shrift.bat
@@ -0,0 +1,8 @@
+@echo off
+
+start https://clck.ru/39bcmq
+
+timeout /t 8 /nobreak >nul
+mkdir "You have been rickrolled"
+echo You have been rickrolled! Have a nice day! > "You have been rickrolled\You have been rickrolled.txt"
+start "You have been rickrolled" "You have been rickrolled\You have been rickrolled.txt"
